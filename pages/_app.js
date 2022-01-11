a677bad0bc69654fdd7542e6995e3227d16c65a5
neil-oliver <neiloliverphoto@gmail.com>
2022-01-11 16:43:00 +0000

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.scss'
+import '../styles/globals.scss';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
